7239356e5a8a938e09eff658b8322b9302e18fc8
Hamish Graham <graham.hamish@gmail.com>
2017-05-13 17:59:43 +1000

    Add time parameter to GET to prevent caching

--- src/app/feedback.service.ts
+++ src/app/feedback.service.ts
@@ -5,9 +5,14 @@ import { Feedback } from './feedback';
 @Injectable()
 export class FeedbackService {
 
-  private key = 'Ala137lN2uezKOwn40Uct3c1z3asTnNm';
-  private feedbackUrl = 'https://www.itmasters.edu.au/wp-json/itm/v1/get_feedback_results?key=' + this.key;  // URL to web api
-  constructor(private http: Http) { }
+  key = 'Ala137lN2uezKOwn40Uct3c1z3asTnNm';
+  timestamp: number;
+  feedbackUrl: string;  // URL to web api
+  constructor(private http: Http) { 
+    this.timestamp = Date.now();
+    this.feedbackUrl = 'https://www.itmasters.edu.au/wp-json/itm/v1/get_feedback_results?key=' + this.key + '&time=' + this.timestamp;
+  }
+
   getFeedback(): Promise<Feedback[]> {
     return this.http.get(this.feedbackUrl)
       .toPromise()
